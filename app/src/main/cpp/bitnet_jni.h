05218fc4633f405f55c0c97af9f27cce9e8ec1d0
simuelunbo <simuelunbo@gmail.com>
2025-05-07 17:48:34 +0900

    Add Bitnet C++ code to Android JNI Connect

---
+++ app/src/main/cpp/bitnet_jni.h
@@ -0,0 +1,14 @@
+//
+// Created by sjb on 2025. 5. 7..
+//
+
+#ifndef BITNET_ANDROID_BITNET_JNI_H
+#define BITNET_ANDROID_BITNET_JNI_H
+
+
+class bitnet_jni {
+
+};
+
+
+#endif //BITNET_ANDROID_BITNET_JNI_H
