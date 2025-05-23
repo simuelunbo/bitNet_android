1beeed3bc49dbdc5d6bee4602b4b1beae9c219cb
sim junbo <46774431+simuelunbo@users.noreply.github.com>
2025-05-23 21:57:45 +0900

    Add splash to chat list navigation

---
+++ app/src/main/java/com/simuel/onebitllm/ui/navigation/BitnetNavGraph.kt
@@ -0,0 +1,47 @@
+package com.simuel.onebitllm.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.simuel.onebitllm.ui.chatlist.ChatListScreen
+import com.simuel.onebitllm.ui.splash.SplashRoute
+
+/**
+ * App navigation graph. Starts at [Splash] and navigates to [ChatList].
+ */
+object BitnetDestination {
+    const val Splash = "splash"
+    const val ChatList = "chat_list"
+}
+
+@Composable
+fun BitnetNavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = BitnetDestination.Splash
+    ) {
+        addSplash(navController)
+        addChatList()
+    }
+}
+
+private fun NavGraphBuilder.addSplash(navController: NavHostController) {
+    composable(BitnetDestination.Splash) {
+        SplashRoute(onNavigateToChatList = {
+            navController.navigate(BitnetDestination.ChatList) {
+                popUpTo(BitnetDestination.Splash) { inclusive = true }
+            }
+        })
+    }
+}
+
+private fun NavGraphBuilder.addChatList() {
+    composable(BitnetDestination.ChatList) {
+        ChatListScreen()
+    }
+}
